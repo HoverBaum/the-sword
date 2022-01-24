23c3673d1c9fcba88985c9fdaf94b45764e6a395
Hendrik <mail@hendrikwallbaum.de>
2022-01-24 22:19:06 +0100

    ✨  add textSpeed config.

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,12 @@
+const baseConfid = {
+  wordFadeInTime: 0.5,
+  wordDelayTime: 0.1,
+}
+
+const textSpeed = 10
+
 export const config = {
   INITIALLY_DISPLAYED_WORDS: 3,
-  WORD_FADE_IN_TIME: 0.5,
-  WORD_DELAY_TIME: 0.1,
+  WORD_FADE_IN_TIME: baseConfid.wordFadeInTime / textSpeed,
+  WORD_DELAY_TIME: baseConfid.wordDelayTime / textSpeed,
 }

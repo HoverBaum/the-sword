206f3d8f74efd9c74cfe949b0b16f25ad0a33c7a
Hendrik <mail@hendrikwallbaum.de>
2022-01-24 15:32:55 +0100

    ✨ add watch option to compile script.

--- scripts/compile.mjs
+++ scripts/compile.mjs
@@ -2,18 +2,20 @@
 
 let IS_WATCHING = false
 
-const { fs } = require('zx')
+if (argv.w || argv.watch) {
+  IS_WATCHING = true
+}
 
-const inkFile = path.join(__dirname, './../story/TheInkSword.ink')
-const jsonFile = path.join(__dirname, './../story/TheInkSword.ink.json')
-const tsFile = path.join(__dirname, './../story/storyContent.ts')
+const inkFile = path.join(__dirname, "./../story/TheInkSword.ink")
+const jsonFile = path.join(__dirname, "./../story/TheInkSword.ink.json")
+const tsFile = path.join(__dirname, "./../story/storyContent.ts")
 
 let inklecatePath
-if (process.platform === 'darwin') {
-  inklecatePath = path.join(__dirname, './../inklecate/mac/inklecate')
+if (process.platform === "darwin") {
+  inklecatePath = path.join(__dirname, "./../inklecate/mac/inklecate")
 }
-if(process.platform === 'win32') {
-  inklecatePath = path.join(__dirname, './../inklecate/win32/inklecate.exe')
+if (process.platform === "win32") {
+  inklecatePath = path.join(__dirname, "./../inklecate/win32/inklecate.exe")
 }
 
 if (!inklecatePath || fs.ensureFileSync(inklecatePath)) {
@@ -24,7 +26,7 @@ if (!inklecatePath || fs.ensureFileSync(inklecatePath)) {
 const compileFile = async (filepath) => {
   console.log(`Compiling ${filepath}`)
   await $`${inklecatePath} ${inkFile}`
-  console.log('Compiled to json')
+  console.log("Compiled to json")
   await createTSFile()
 }
 
@@ -32,18 +34,18 @@ const createTSFile = async () => {
   const jsonContent = await fs.readFile(jsonFile)
   const tsContent = `export const storyContent = ${jsonContent}`
   await fs.writeFile(tsFile, tsContent)
-  console.log('TS file updated')
+  console.log("TS file updated")
 }
 
-console.log('Compiling initial json')
+console.log("Compiling initial json")
 await compileFile(inkFile)
 
-if(IS_WATCHING) {
+if (IS_WATCHING) {
   fs.watchFile(inkFile, { interval: 500 }, (curr, prev) => {
     console.log(`\n${inkFile} Changed`)
-  
+
     // Compile the story to json using inklecate
     compileFile(inkFile)
   })
-  console.log('\nWaiting for changes')
-  }
+  console.log("\nWaiting for changes")
+}

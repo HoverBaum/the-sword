81bfce6cdaeae3f2b85fdd099d2a7a7d8ee12f1c
Hendrik Wallbaum <mail@hendrikwallbaum.de>
2022-03-07 00:48:25 +0100

    🎨 improve button readability.

--- src/Story/ChoiceButton.tsx
+++ src/Story/ChoiceButton.tsx
@@ -1,7 +1,8 @@
 /** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react'
 import { Button, ButtonProps, Spacer } from '@geist-ui/core'
 import { ChevronDown } from '@geist-ui/icons'
-import { ComponentType } from 'react'
+import { ComponentType, useEffect, useRef, useState } from 'react'
 import { pageTurnSoundAsset } from '../assets'
 import { useSound } from '../useSound'
 
@@ -18,6 +19,16 @@ export const ChoiceButton: ComponentType<ChoiceButtonProps> = ({
 }) => {
   const isContinue = /continue/i.test(text)
   const { playSoundEffect } = useSound()
+  const buttonRef = useRef<HTMLButtonElement | null>(null)
+
+  // For readability we make text bold on focus and hover.
+  // To reduce motion we figure out the bold width and force
+  // buttons to use that.
+  const [width, setWidth] = useState(0)
+  useEffect(() => {
+    if (!buttonRef.current) return
+    setWidth(buttonRef.current.clientWidth)
+  }, [buttonRef.current])
 
   const playFocusSound = () => {
     playSoundEffect(pageTurnSoundAsset)
@@ -33,11 +44,40 @@ export const ChoiceButton: ComponentType<ChoiceButtonProps> = ({
     ghost: true,
   }
 
+  console.log('Width', width)
+
   return (
-    <div>
+    <div
+      css={css`
+        & button:hover,
+        & button:focus {
+          font-weight: bold;
+        }
+        // Here is the "magic" to force the right width.
+        ${width > 0 &&
+        css`
+          & button.btn {
+            width: ${width}px;
+            display: flex;
+          }
+        `}
+        // We need initially bold text to get the "bol width".
+        ${width === 0 &&
+        css`
+          & button.btn {
+            font-weight: bold;
+          }
+        `}
+      `}
+    >
       <Spacer h={2} />
-      {!isContinue && <Button {...buttonProps}>{text}</Button>}
+      {!isContinue && (
+        <Button ref={buttonRef} {...buttonProps}>
+          {text}
+        </Button>
+      )}
       {isContinue && (
+        // Continue buttons don't need bold magic.
         <Button {...buttonProps} icon={<ChevronDown />} auto></Button>
       )}
     </div>

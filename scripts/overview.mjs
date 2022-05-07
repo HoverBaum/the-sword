16fe500e7ac3b1f22eedf3c5a8875aeda9ca7969
Hendrik Wallbaum <mail@hendrikwallbaum.de>
2022-05-07 10:13:24 +0200

    ✨ can turn mermaid into image.

--- scripts/overview.mjs
+++ scripts/overview.mjs
@@ -1,5 +1,16 @@
 #!/usr/bin/env zx
 
 console.log('Generating overview 🗺')
+const tmpMermaidFile = 'story/overview.mmd'
 
 // Let's turn the ink files into a mermaid diagram.
+const mermaidDiagram = `graph TD;
+  A-->B;
+  A-->C;
+  B-->D;
+  C-->D;`
+
+// Turn the mermaid dinagram into an image we can view.
+await fs.writeFile(tmpMermaidFile, mermaidDiagram)
+
+$`npx mmdc -i ${tmpMermaidFile} -b transparent -o story/overview.svg`

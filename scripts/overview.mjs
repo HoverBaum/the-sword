131fdf6cb51b87a23f88b127d54482a50b2c4adb
Hendrik Wallbaum <mail@hendrikwallbaum.de>
2022-05-07 23:39:57 +0200

    ✨ generate basic overview from story json.

--- scripts/overview.mjs
+++ scripts/overview.mjs
@@ -4,13 +4,38 @@ console.log('Generating overview 🗺')
 const tmpMermaidFile = 'story/overview.mmd'
 
 // Let's turn the ink files into a mermaid diagram.
-const mermaidDiagram = `graph TD;
-  A-->B;
-  A-->C;
-  B-->D;
-  C-->D;`
+// We probably want to parse the json for this.
+// Looks liek we can search for "->" attributes and ann values that don't start with a "." should generate a new connection.
+// Like always remember which knot we are currently in, search for arrows and generate a transition for each arrow found.
+const parsedInk = await fs.readJson('story/TheInkSword.ink.json')
+
+console.log(Object.keys(parsedInk.root[2]))
+
+const transitions = []
+
+const knotsObject = parsedInk.root[2]
+Object.keys(knotsObject).forEach((knot) => {
+  const knotObject = knotsObject[knot]
+  const jsonKnot = JSON.stringify(knotObject, null, 2)
+  jsonKnot
+    .split('\n')
+    .filter((line) => line.includes('"->"'))
+    .forEach((line) => {
+      const destination = line.split(':')[1].trim().replace(/"/g, '')
+      if (/^[\.\$]/.test(destination)) return
+      transitions.push({ from: knot, to: destination })
+    })
+})
+
+console.log(transitions)
+
+const mermaidDiagram = `graph TD;${transitions
+  .map(({ from, to }) => `${from}-->${to}`)
+  .join(';')};`
+
+console.log(mermaidDiagram)
 
 // Turn the mermaid dinagram into an image we can view.
 await fs.writeFile(tmpMermaidFile, mermaidDiagram)
 
-$`npx mmdc -i ${tmpMermaidFile} -b transparent -o story/overview.svg`
+$`npx mmdc -i ${tmpMermaidFile} -o story/overview.svg`
